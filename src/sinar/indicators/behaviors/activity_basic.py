5dd9e58c4488deea3bec259bf14d721e6577beb9
Khairil Yusof <khairil.yusof@gmail.com>
2022-10-13 14:06:00 +0800

    Add participant number by gender behavior under new Indicators tab

--- src/sinar/indicators/behaviors/activity_basic.py
+++ src/sinar/indicators/behaviors/activity_basic.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from plone import schema
+from plone.autoform import directives
 from plone.autoform.interfaces import IFormFieldProvider
 from plone.supermodel import model
+from plone.supermodel.directives import fieldset
 from Products.CMFPlone.utils import safe_hasattr
 from zope.component import adapter
 from zope.interface import Interface, implementer, provider
@@ -19,11 +21,37 @@ class IActivityBasic(model.Schema):
     """
     """
 
-    project = schema.TextLine(
-        title=_(u'Project'),
-        description=_(u'Give in a project name'),
+    activity_participants_male = schema.Int(
+        title=_(u'Male Participants'),
+        description=_(u'Number of Participants'),
         required=False,
     )
+
+    activity_participants_female = schema.Int(
+        title=_(u'Female Participants'),
+        description=_(u'Number of Participants'),
+        required=False,
+    )
+
+    activity_participants_other= schema.Int(
+        title=_(u'Other Participants'),
+        description=_(u'Number of non-Binary Participants'),
+        required=False,
+    )
+
+    # fieldset set the tabs on the edit form
+
+    fieldset(
+            'indicators',
+            label=_(u'Indicators'),
+            fields=[
+                'activity_participants_male',
+                'activity_participants_female',
+                'activity_participants_other',
+                ],
+            )
+
+
 
 
 @implementer(IActivityBasic)
@@ -33,11 +61,31 @@ class ActivityBasic(object):
         self.context = context
 
     @property
-    def project(self):
-        if safe_hasattr(self.context, 'project'):
-            return self.context.project
+    def activity_participants_male(self):
+        if safe_hasattr(self.context, 'activity_participants_male'):
+            return self.context.activity_participants_male
         return None
 
-    @project.setter
-    def project(self, value):
-        self.context.project = value
+    @activity_participants_male.setter
+    def activity_participants_male(self, value):
+        self.context.activity_participants_male = value
+
+    @property
+    def activity_participants_female(self):
+        if safe_hasattr(self.context, 'activity_participants_female'):
+            return self.context.activity_participants_female
+        return None
+
+    @activity_participants_female.setter
+    def activity_participants_female(self, value):
+        self.context.activity_participants_female = value
+
+    @property
+    def activity_participants_other(self):
+        if safe_hasattr(self.context, 'activity_participants_other'):
+            return self.context.activity_participants_other
+        return None
+
+    @activity_participants_other.setter
+    def activity_participants_other(self, value):
+        self.context.activity_participants_other = value
